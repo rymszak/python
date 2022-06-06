3cfe0a306f46f779fd89a6e71b61e3ca2d61ef12
rymszak <102522109+rymszak@users.noreply.github.com>
2022-06-06 13:30:53 +0200

    code

--- code.py
+++ code.py
@@ -1,3 +1,20 @@
-a=4
-b=5
-print("a+b")
+print("podaj liczbe")
+a=float(input())
+print("podaj liczbe 2")
+b=float(input())
+def summa(a,b):
+    suma=a + b
+    print("suma to", suma)
+def roznicca(a,b):
+    roznica=a -b
+    print("roznica to", roznica)
+def iloczynn(a,b)
+    iloczyn=a*b
+    print("iloczyn to", iloczyn)
+def ilorazz(a,b)
+    iloraz=a:b
+    print("iloraz to", iloraz)
+summa(a,b)
+roznicca(a,b)
+iloczynn(a,b)
+ilorazz(a,b)
